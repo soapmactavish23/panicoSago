a9ada2597514ca16e7f6653572f1e61d41ecc1ac
soapmactavish23 <ricknogueira1231@gmail.com>
2020-08-20 14:43:56 -0300

    '0.0.4'

--- api/class/alerta.php
+++ api/class/alerta.php
@@ -2,16 +2,64 @@
 
 class alerta extends database {
 
+	public function obterTodos() {
+		$sql = "SELECT * FROM alerta";
+	
+		if ( $rs = parent::fetch_all($sql) ) {
+			foreach ( $rs as $row ) {
+				$col = array();
+				foreach ( $row as $k=>$v ) {
+					$col[$k] = stripslashes($v);
+				}
+				$rows[] = $col;
+			}
+			return array( 'data' => $rows );
+		}
+	}
+	
+	public function obterTodosDoCliente() {
+		global $_user;
+		$sql = "SELECT * FROM alerta WHERE idcliente = ".$_user->idcliente;
+	
+		if ( $rs = parent::fetch_all($sql) ) {
+			foreach ( $rs as $row ) {
+				$col = array();
+				foreach ( $row as $k=>$v ) {
+					$col[$k] = stripslashes($v);
+				}
+				$rows[] = $col;
+			}
+			return array( 'data' => $rows );
+		}
+	}
+
+	public function obterTodosDoUsuario() {
+		global $_user;
+		$sql = "SELECT * FROM alerta WHERE idusuario = ".$_user->idusuario;
+	
+		if ( $rs = parent::fetch_all($sql) ) {
+			foreach ( $rs as $row ) {
+				$col = array();
+				foreach ( $row as $k=>$v ) {
+					$col[$k] = stripslashes($v);
+				}
+				$rows[] = $col;
+			}
+			return array( 'data' => $rows );
+		}
+	}
+
 	public function salvar() {
-        // variaveis da denuncia
+		global $_user;
+		// variaveis da denuncia
+		$this->idcliente = $_user->idcliente;
 		$this->nome = addslashes(@ $_REQUEST['nome']);
 		$this->latitude = (@ $_REQUEST['latitude']);
-        $this->longitude = (@ $_REQUEST['longitude']);
-        
+		$this->longitude = (@ $_REQUEST['longitude']);
+		$this->dt_registro = (@ $_REQUEST['dt_registro']);
 		// grava alerta
         $this->idalerta = $this->insert();
-
-        return array ( 'idalerta' => $this->idalerta );
+		return array ( 'idalerta' => $this->idalerta );
 		
 	}
 
